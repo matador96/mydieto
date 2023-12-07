b3b91ae9febf4362ba8b3c4d2f656587e4903945
altunko <altun@ecorium.tech>
2023-12-07 18:51:34 +0700

    little

--- backend/api/server.js
+++ backend/api/server.js
@@ -16,14 +16,14 @@ app.use(
 );
 
 // Cors disable
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept",
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
